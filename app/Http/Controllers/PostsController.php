a497dc39505ba5a841dca61a9799005097bebb03
s3a317097 <s3a317097@users.noreply.github.com>
2017-11-19 15:56:57 +0800

    設定controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,5 +8,20 @@ use App\Http\Requests;
 
 class PostsController extends Controller
 {
-    //
+    public function index(){
+        return view('index');
+    }
+    public function show(){
+        return view('post');
+
+    }
+    public function about(){
+        return view('about');
+
+    }
+    public function contact(){
+        return view('contact');
+
+    }
+
 }
